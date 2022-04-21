0f1111e7f884a4b1f482c7bb2baedff5084d2377
Holvert-P <holvertperez@hotmail.com>
2022-04-20 20:52:31 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-zaa8y9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-zaa8y9)
---
+++ index.ts
@@ -0,0 +1,9 @@
+const superHeroe = {
+  nombre: 'Spiderman',
+  edad: 30,
+  direccion: {
+    calle: 'Main St',
+    pais: 'USA',
+    ciudad: 'NY',
+  },
+};
